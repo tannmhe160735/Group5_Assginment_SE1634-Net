d6a2ab4dc5fa5218d9c089afefc234710d6d5913
tannmhe160735 <105600279+tannmhe160735@users.noreply.github.com>
2022-10-01 11:32:15 +0700

    Servlet and DBcontext

---
+++ SWP_Assignment_Group_5/src/java/DAO/DBContext.java
@@ -0,0 +1,38 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package DAO;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+/**
+ *
+ * @author Ngo Tung Son
+ */
+public abstract class DBContext<T> {
+    protected Connection connection;
+    public DBContext()
+    {
+        try {
+            String user = "tan";
+            String pass = "12345";
+            String url = "jdbc:sqlserver://localhost\\SQLEXPRESS:1433;databaseName=ProjectWeb";
+            Class.forName("com.microsoft.sqlserver.jdbc.SQLServerDriver");
+            connection = DriverManager.getConnection(url, user, pass);
+        } catch (ClassNotFoundException | SQLException ex) {
+            Logger.getLogger(DBContext.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }
+    
+    public abstract ArrayList<T> list();
+    public abstract T get(int id);
+    public abstract void insert(T model);
+    public abstract void update(T model);
+    public abstract void delete(T model);
+}

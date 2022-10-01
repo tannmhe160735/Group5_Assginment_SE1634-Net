a7e71bc8630f930bf11a3e2f677d0d2ebff4c95d
tannmhe160735 <105600279+tannmhe160735@users.noreply.github.com>
2022-10-01 19:38:46 +0700

    Update DemoServlet.java

--- SWP_Assignment_Group_5/src/java/Controller/DemoServlet.java
+++ SWP_Assignment_Group_5/src/java/Controller/DemoServlet.java
@@ -53,7 +53,7 @@ public class DemoServlet extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response)
     throws ServletException, IOException {
-        response.sendRedirect("home.jsp");
+        response.sendRedirect("detail.jsp");
     } 
 
     /** 

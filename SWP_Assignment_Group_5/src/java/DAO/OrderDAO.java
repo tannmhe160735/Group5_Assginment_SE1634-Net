b7df6ffb8e31dd7a0ffdfd7789abad47000b9b44
tannmhe160735 <105600279+tannmhe160735@users.noreply.github.com>
2022-11-02 18:54:46 +0700

    order for admin

--- SWP_Assignment_Group_5/src/java/DAO/OrderDAO.java
+++ SWP_Assignment_Group_5/src/java/DAO/OrderDAO.java
@@ -20,65 +20,88 @@ import java.util.logging.Logger;
  * @author Admin
  */
 public class OrderDAO {
-     Connection conn = DB.Binh_DBContext.CreateConnection();
-     public List<OrderDetail> GetAllOrder(){
-         List<OrderDetail> list = new ArrayList<OrderDetail>();
-         String sql = "select * from [order]";
-         String listOrder = "select * from order_detail where order_id = ?";
-         try {
-             PreparedStatement ptmt = conn.prepareStatement(sql);
-             ResultSet rs = ptmt.executeQuery();
-             while(rs.next()){
-                 List<Order> orders = new ArrayList<Order>();
-                 OrderDetail order = OrderDetail.builder()
-                         .ord_id(rs.getInt("order_id"))
-                         .user_id(rs.getInt("user_id"))
-                         .address(rs.getString("address"))
-                         .phone(rs.getString("phone"))
-                         .email(rs.getString("email"))
-                         .payment(rs.getString("payment"))
-                         .status(rs.getBoolean("status")).build()
-                         ;
 
-                 PreparedStatement ptmtOrder = conn.prepareStatement(listOrder);
-                 ptmtOrder.setInt(1, order.getOrd_id());
-                 ResultSet rsOrders = ptmtOrder.executeQuery();
-                 while(rsOrders.next()){
-                     orders.add(Order.builder()
-                             .ord_id(rsOrders.getInt("order_id"))
-                             .prod_id(rsOrders.getInt("product_id"))
-                             .quantity(rsOrders.getInt("quantity"))
-                             .build());
-                 }
-                 order.setListOrders(orders);
-                 list.add(order);
-                
-             }
-              
-         } catch (SQLException ex) {
-             ex.printStackTrace();
-         }
-         
-         
-         return list;
-     }
-     public static void main(String[] args) {
-         OrderDAO dao = new OrderDAO();
-          List<OrderDetail> list = dao.GetAllOrder();
-            for (OrderDetail order : list) {
-                System.out.println("OrderID: "+order.getOrd_id());
-                System.out.println("UserID: "+order.getUser_id());
-                System.out.println("Email: "+order.getEmail());
-                System.out.println("Adress: "+order.getAddress());
-                System.out.println("Phone: "+order.getPhone());
-                System.out.println("Payment Method:"+order.getPayment());
-                System.out.println("Order Status: "+order.isStatus());
-                System.out.println("List Order Product:");
-                for (Order orderDetail : order.getListOrders()) {
-                    System.out.println(orderDetail.getProd_id()+": "+orderDetail.getQuantity());
+    Connection conn = DB.Binh_DBContext.CreateConnection();
+
+    public List<OrderDetail> GetAllOrder() {
+        List<OrderDetail> list = new ArrayList<OrderDetail>();
+        String sql = "select * from [order]";
+        String listOrder = "select * from order_detail where order_id = ?";
+        try {
+            PreparedStatement ptmt = conn.prepareStatement(sql);
+            ResultSet rs = ptmt.executeQuery();
+            while (rs.next()) {
+                List<Order> orders = new ArrayList<Order>();
+                OrderDetail order = OrderDetail.builder()
+                        .ord_id(rs.getInt("order_id"))
+                        .user_id(rs.getInt("user_id"))
+                        .address(rs.getString("address"))
+                        .phone(rs.getString("phone"))
+                        .email(rs.getString("email"))
+                        .payment(rs.getString("payment"))
+                        .status(rs.getBoolean("status")).build();
+
+                PreparedStatement ptmtOrder = conn.prepareStatement(listOrder);
+                ptmtOrder.setInt(1, order.getOrd_id());
+                ResultSet rsOrders = ptmtOrder.executeQuery();
+                while (rsOrders.next()) {
+                    orders.add(Order.builder()
+                            .ord_id(rsOrders.getInt("order_id"))
+                            .prod_id(rsOrders.getInt("product_id"))
+                            .quantity(rsOrders.getInt("quantity"))
+                            .build());
                 }
-                System.out.println("");
+                order.setListOrders(orders);
+                list.add(order);
+
             }
+
+        } catch (SQLException ex) {
+            ex.printStackTrace();
+        }
+
+        return list;
+    }
+
+    public List<OrderDetail> GetOrderByMonth() {
+        List<OrderDetail> OrderDetails = new ArrayList<OrderDetail>();
+        try {
+            String sql = "select P.product_id, D.quantity, O.order_id "
+                    + "from [order] O, order_detail D  , product P\n"
+                    + "where O.order_id = D.order_id and D.product_id = P.product_id and MONTH(O.date_created) = 11 and YEAR(O.date_created) = 2022";
+            PreparedStatement stm = conn.prepareStatement(sql);
+            ResultSet rs = stm.executeQuery();
+            while (rs.next()) {
+                OrderDetail order = OrderDetail.builder()
+                        .ord_id(rs.getInt("order_id"))
+                        .user_id(rs.getInt("user_id"))
+                        .address(rs.getString("address"))
+                        .phone(rs.getString("phone"))
+                        .email(rs.getString("email"))
+                        .payment(rs.getString("payment"))
+                        .status(rs.getBoolean("status")).build();
+            }
+        } catch (SQLException ex) {
+            ex.printStackTrace();
         }
     }
 
+    public static void main(String[] args) {
+        OrderDAO dao = new OrderDAO();
+        List<OrderDetail> list = dao.GetAllOrder();
+        for (OrderDetail order : list) {
+            System.out.println("OrderID: " + order.getOrd_id());
+            System.out.println("UserID: " + order.getUser_id());
+            System.out.println("Email: " + order.getEmail());
+            System.out.println("Adress: " + order.getAddress());
+            System.out.println("Phone: " + order.getPhone());
+            System.out.println("Payment Method:" + order.getPayment());
+            System.out.println("Order Status: " + order.isStatus());
+            System.out.println("List Order Product:");
+            for (Order orderDetail : order.getListOrders()) {
+                System.out.println(orderDetail.getProd_id() + ": " + orderDetail.getQuantity());
+            }
+            System.out.println("");
+        }
+    }
+}
